5948b2d3d0cd0acc9aa8a4f4795759be6f0ae640
Arpita Biswal <arpitabiswal777@gmail.com>
2020-03-16 22:35:10 +0530

    First commit

---
+++ src/page/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from  'react';
+import Directory from '../../component/Directory/directory'
+import './homepage.styles.css' ;
+
+const HomePage = () => {
+	return(
+		<Directory />
+		)
+}
+
+export default HomePage;
